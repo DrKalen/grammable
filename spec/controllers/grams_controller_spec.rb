8c4b6cb4b86e9b9aa4e33caf967936ba857eedc1
DrKalen <kalen7@gmail.com>
2017-04-03 16:09:55 +0000

    Added model and initial test for working http request to grams controller

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,10 +1,21 @@
 require 'rails_helper'
 
+RSpec.describe Gram, type: :model do
+
+end
+
 RSpec.describe GramsController, type: :controller do
     describe "grams#index action" do
         it "should successfully show the page" do
         get :index
         expect(response).to have_http_status(:success)
+        end
+    end
+
+    describe "grams#new action" do
+        it "should sucessfully show the new form" do
+            get:new
+            expect(response).to have_http_status(:success)
         end
     end
 end
