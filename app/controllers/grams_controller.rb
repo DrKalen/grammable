8f1280b613479da2f8e597a98b0781e6d8449f20
DrKalen <kalen7@gmail.com>
2017-04-11 22:00:02 +0000

    Successfully added CarrierWave image uploading and tested it. Added figaro and fog and configured the switch to AWS, but when I tried to upload the same picture, got error Connection reset by peer...

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -34,6 +34,7 @@ class GramsController < ApplicationController
     end
 
     def index
+        @grams = Gram.all
     end
 
     def new
@@ -50,7 +51,7 @@ class GramsController < ApplicationController
     end
 
     def gram_params
-        params.require(:gram).permit(:message)
+        params.require(:gram).permit(:message, :picture)
     end
 
     def render_not_found(status=:not_found)
