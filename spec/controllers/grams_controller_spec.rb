729dadfcebf00f50482d8722d2afc094141a4e60
DrKalen <kalen7@gmail.com>
2017-04-13 21:21:20 +0000

    Added model and controller for comments and wrote specs for create action on index page.

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -141,7 +141,7 @@ RSpec.describe GramsController, type: :controller do
 
     describe "grams#create action" do
         it "should require users to be logged in" do 
-            post :create, params:{ gram:{ message: "Hello"}} 
+            post :create, params:{ gram:{ message: "Hello" } } 
             expect(response).to redirect_to new_user_session_path
         end
 
@@ -149,7 +149,13 @@ RSpec.describe GramsController, type: :controller do
             user = FactoryGirl.create(:user)
             sign_in user
                            
-            post :create, params: { gram: { message: 'Hello!'}}
+             post :create, params:{ 
+                gram:{ 
+                    message: "Hello!", 
+                    picture: fixture_file_upload("/picture.png", 'image/png')
+                }
+              } 
+
             expect(response).to redirect_to root_path
 
             gram = Gram.last
